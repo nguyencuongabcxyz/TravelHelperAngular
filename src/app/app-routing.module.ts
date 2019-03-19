55beb923abf440a5b8556d57d5cd9deb456d353c
quangtl322 <48589566+quangtl322@users.noreply.github.com>
2019-03-19 15:51:14 +0700

    userAuth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
+import{AuthGuard} from './auth/auth.guard'
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { UserComponent } from './components/user/user.component';
+import { DashboardComponent } from './components/user/dashboard/dashboard.component';
+import { UserauthComponent } from './components/userauth/userauth.component';
+import { SigninComponent } from './components/userauth/signin/signin.component';
+import { SignupComponent } from './components/userauth/signup/signup.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/Userauth/SignIn', pathMatch: 'full' },
+  {
+    path: 'Userauth', component: UserauthComponent,
+    children: [
+      { path: '', redirectTo: '/Userauth/SignIn', pathMatch: 'full' },
+      { path: 'SignUp', component: SignupComponent },
+      { path: 'SignIn', component: SigninComponent }
+    ]
+  },
+  {
+    path: 'Users', component: UserComponent,canActivate:[AuthGuard],
+    children: [
+      { path: '', redirectTo: '/Users/Dashboard', pathMatch: 'full' },
+      { path: 'Dashboard', component: DashboardComponent },
+      { path: 'Profile', component: DashboardComponent },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
